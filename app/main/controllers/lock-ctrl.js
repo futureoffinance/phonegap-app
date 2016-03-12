c7cfb8d7691a2190eb72633b1a6457633e912ac0
Matiss Janis Aboltins <matiss@mja.lv>
2016-03-12 17:00:50 +0200

    Mino imrovement

--- app/main/controllers/lock-ctrl.js
+++ app/main/controllers/lock-ctrl.js
@@ -4,10 +4,12 @@ angular.module('main')
 
   $log.log('Lock Screen');
 
-  window.plugins.touchid.verifyFingerprint(
-    'Scan your fingerprint please', // this will be shown in the native scanner popup
-     function (msg) {alert('ok: ' + msg);}, // success handler: fingerprint accepted
-     function (msg) {alert('not ok: ' + JSON.stringify(msg));} // error handler with errorcode and localised reason
-  );
+  if (window.plugins && window.plugins.touchid) {
+    window.plugins.touchid.verifyFingerprint(
+      'Scan your fingerprint please', // this will be shown in the native scanner popup
+       function (msg) {alert('ok: ' + msg);}, // success handler: fingerprint accepted
+       function (msg) {alert('not ok: ' + JSON.stringify(msg));} // error handler with errorcode and localised reason
+    );
+  }
 
 });
