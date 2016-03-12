eb0efa054e018d4c92aa83703df18c8103bc9757
Matiss Janis Aboltins <matiss@mja.lv>
2016-03-12 16:41:17 +0200

    Home screen

--- app/main/controllers/home-ctrl.js
+++ app/main/controllers/home-ctrl.js
@@ -1,7 +1,49 @@
 'use strict';
 angular.module('main')
-.controller('HomeCtrl', function ($log) {
+.controller('HomeCtrl', function ($scope) {
 
-  $log.log('Homepage');
+  $scope.transactions = [{
+    id: 1,
+    group: 'today',
+    amount: 5.99,
+    description: 'Starbucks',
+    icon: 'ion-coffee'
+  }, {
+    id: 2,
+    group: 'today',
+    amount: 10.00,
+    description: 'Hardware store',
+    icon: 'ion-hammer'
+  }, {
+    id: 3,
+    group: 'yesterday',
+    amount: 10.00,
+    description: 'Hardware store',
+    icon: 'ion-hammer'
+  }, {
+    id: 4,
+    group: 'yesterday',
+    amount: 50.00,
+    description: 'Hardware store',
+    icon: 'ion-hammer'
+  }, {
+    id: 5,
+    group: 'yesterday',
+    amount: 1101.92,
+    description: 'Hardware store',
+    icon: 'ion-hammer'
+  }, {
+    id: 6,
+    group: 'today',
+    amount: 59.99,
+    description: 'Starbucks',
+    icon: 'ion-coffee'
+  }, {
+    id: 7,
+    group: 'today',
+    amount: 1101.92,
+    description: 'Hardware store',
+    icon: 'ion-hammer'
+  }];
 
 });
