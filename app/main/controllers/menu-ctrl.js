b6fb106d02a8e86364229c32e9a9cce368a029fa
Matiss Janis Aboltins <matiss@mja.lv>
2016-03-13 10:45:20 +0200

    Improvements

--- app/main/controllers/menu-ctrl.js
+++ app/main/controllers/menu-ctrl.js
@@ -8,14 +8,14 @@ angular.module('main')
     }
 
     $scope.scheduleDelayedNotification = function () {
-      // var now = new Date().getTime();
-      // var _10SecondsFromNow = new Date(now + 10 * 1000);
+      var now = new Date().getTime();
+      var _10SecondsFromNow = new Date(now + 5 * 1000);
 
       $cordovaLocalNotification.schedule({
         id: 1,
         title: 'Title here',
         text: 'Text here',
-        // at: _10SecondsFromNow
+        at: _10SecondsFromNow
       }).then(function () {
         // ...
       });
